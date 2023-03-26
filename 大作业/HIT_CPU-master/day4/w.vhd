df550fc03fc6519c08379333d0818b8326e911bf
Jiuyao Qu <2668136485@qq.com>
2023-03-27 00:04:15 +0800

    first commit

---
+++ 大作业/HIT_CPU-master/day4/w.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:04:31 12/03/2013 
+-- Design Name: 
+-- Module Name:    w - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity w is
+end w;
+
+architecture Behavioral of w is
+
+begin
+
+
+end Behavioral;
+
